2fe4413a82ac2bf112e6e6502d75d0d54eb836e2
ShehanSH <shehanhashen928@gmail.com>
2023-07-09 15:10:51 +0530

    change product funality

--- backend/model/product.js
+++ backend/model/product.js
@@ -28,3 +28,57 @@ const productSchema = new mongoose.Schema({
     required: [true, "Please enter your product stock!"],
   },
 
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  reviews: [
+    {
+      user: {
+        type: Object,
+      },
+      rating: {
+        type: Number,
+      },
+      comment: {
+        type: String,
+      },
+      productId: {
+        type: String,
+      },
+      createdAt:{
+        type: Date,
+        default: Date.now(),
+      }
+    },
+  ],
+  ratings: {
+    type: Number,
+  },
+  shopId: {
+    type: String,
+    required: true,
+  },
+  shop: {
+    type: Object,
+    required: true,
+  },
+  sold_out: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);

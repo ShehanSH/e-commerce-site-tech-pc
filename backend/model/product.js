d998ce8c9713a18e59a8ad9b7bc5c34c306a0081
ShehanSH <shehanhashen928@gmail.com>
2023-07-09 15:04:17 +0530

    create product

---
+++ backend/model/product.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your product name!"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter your product description!"],
+  },
+  category: {
+    type: String,
+    required: [true, "Please enter your product category!"],
+  },
+  tags: {
+    type: String,
+  },
+  originalPrice: {
+    type: Number,
+  },
+  discountPrice: {
+    type: Number,
+    required: [true, "Please enter your product price!"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter your product stock!"],
+  },
+

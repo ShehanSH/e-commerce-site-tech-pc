eed9abf5f0e7fc16ea762229ed307c83cfc3f02d
ShehanSH <shehanhashen928@gmail.com>
2023-08-03 22:26:38 +0530

    changes send mail

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,15 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const sendMail = async (options) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMPT_HOST,
-        port: process.env.SMPT_PORT,
-        service: process.env.SMPT_SERVICE,
-        auth:{
-            user: process.env.SMPT_MAIL,
-            pass: process.env.SMPT_PASSWORD,
-        },
-    });
+    
 
     const mailOptions = {
         from: process.env.SMPT_MAIL,
@@ -19,6 +11,16 @@ const sendMail = async (options) => {
     };
 
     await transporter.sendMail(mailOptions);
+
+const transporter = nodemailer.createTransport({
+        host: process.env.SMPT_HOST,
+        port: process.env.SMPT_PORT,
+        service: process.env.SMPT_SERVICE,
+        auth:{
+            user: process.env.SMPT_MAIL,
+            pass: process.env.SMPT_PASSWORD,
+        },
+    });
 };
 
 module.exports = sendMail;
